cb507c68f68bc66ae56f7ebf65cd74d1a96e3701
佐藤慧 <kei@h225.249.225.10.1017429.vlan.kuins.net>
2021-03-18 01:08:05 +0900

    pE lesson 331-346 modified

--- miscs/plainEnglish/334.R
+++ miscs/plainEnglish/334.R
@@ -10,6 +10,7 @@ scriptPageHTML <- read_html(scriptPageURL)
 
 scriptRaw <- "/html/body/div[1]/div/div/div/div/main/div/div/section[5]/div/div/div/div/div/section/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div[1]/p"
 
+
 # content -----------------------------------------------------------------
 script <- html_nodes(scriptPageHTML, xpath = scriptRaw) %>% html_text()
 script[1] <- script[1] %>% str_c(., ".")
@@ -17,17 +18,18 @@ script[1] <- script[1] %>% str_c(., ".")
 allInOne <- script %>% str_c(collapse = " ")
 sentences <- tokenizers::tokenize_sentences(allInOne)
 sentences <- sentences[[1]]
-### 以下をリロードすればよろし
-# sentences <- sentences[[1]][1:60]
 
+
+# split each sentence by word ---------------------------------------------
 words <- str_split(sentences, pattern = "[\x20\t]+")
 firstWords <- rep(NA, length(words))
 for (i in 1:length(firstWords)) {
   firstWords[i] <- str_c(words[[i]][1:2], collapse = " ")
 }
 
-table <- data.frame(firstWords, sentences)
 
-library(knitr)
-output <- kable(table, format = "markdown", col.names = c("First Two Words", "Sentence"))
-cat(output, file = str_c("lesson/", lessonNum, ".md"), fill = TRUE)
+# output ------------------------------------------------------------------
+table <- data.frame(firstWords, sentences)
+body <- knitr::kable(table, format = "markdown", col.names = c("First Two Words", "Sentence"))
+title <- "## [Plain English Lesson 334](https://PlainEnglish.com/334/)"
+cat(title, body, file = str_c("lesson/", lessonNum, ".md"), fill = TRUE)
--- miscs/plainEnglish/function.R
+++ miscs/plainEnglish/function.R
@@ -1,8 +1,7 @@
 PETable <- function(lessonNum) {
   # preface -----------------------------------------------------------------
   library(rvest)
-  num <- lessonNum
-  frontPageURL <- str_c("https://plainenglish.com/number/", num, "/")
+  frontPageURL <- str_c("https://plainenglish.com/number/", lessonNum, "/")
   frontPageHTML <- read_html(frontPageURL)
   
   link <- "/html/body/div[1]/div/div/div/div/div/section[2]/div/div/div/div/div/div[2]/div/div/article/div/a/@href"
@@ -11,6 +10,7 @@ PETable <- function(lessonNum) {
   
   scriptRaw <- "/html/body/div[1]/div/div/div/div/main/div/div/section[5]/div/div/div/div/div/section/div/div/div[1]/div/div/div[2]/div/div/div/div[2]/div[1]/p"
   
+  
   # content -----------------------------------------------------------------
   script <- html_nodes(scriptPageHTML, xpath = scriptRaw) %>% html_text()
   script[1] <- script[1] %>% str_c(., ".")
@@ -18,18 +18,22 @@ PETable <- function(lessonNum) {
   allInOne <- script %>% str_c(collapse = " ")
   sentences <- tokenizers::tokenize_sentences(allInOne)
   sentences <- sentences[[1]]
-
-  # split each sentence by word
+  
+  
+  # split each sentence by word ---------------------------------------------
   words <- str_split(sentences, pattern = "[\x20\t]+")
   firstWords <- rep(NA, length(words))
   for (i in 1:length(firstWords)) {
     firstWords[i] <- str_c(words[[i]][1:2], collapse = " ")
   }
   
+
+  # output ------------------------------------------------------------------
   table <- data.frame(firstWords, sentences)
-  
-  output <- knitr::kable(table, format = "markdown", col.names = c("First Two Words", "Sentence"))
-  cat(output, file = str_c("lesson/", lessonNum, ".md"), fill = TRUE)
+  body <- knitr::kable(table, format = "markdown", col.names = c("First Two Words", "Sentence"))
+  title <- str_c("## [Plain English Lesson ", lessonNum, "](https://PlainEnglish.com/",
+                 lessonNum, "/)")
+  cat(title, body, file = str_c("lesson/", lessonNum, ".md"), fill = TRUE)
 }
 
 multiPETable <- function(lessonNums){
@@ -38,4 +42,4 @@ multiPETable <- function(lessonNums){
   }
 }
 
-multiPETable(lessonNums = 301:346)
+multiPETable(lessonNums = 331:346)
