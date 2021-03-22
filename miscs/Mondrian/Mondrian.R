27fbbc829748ab92d774c0af93d9c09ef464c92b
Kei Sato <sato.kei.mail@gmail.com>
2021-03-22 13:53:53 +0900

    ideas dir added

--- miscs/Mondrian/Mondrian.R
+++ miscs/Mondrian/Mondrian.R
@@ -1,15 +1,12 @@
 x <- 1:100
-# y <- seq(1, 200, 2)
+y <- 1:100
+df <- tibble(x = x, y = y)
 
-df <- tibble(x = x, y = x)
-
-df %>% 
-  ggplot(mapping = aes(x = x, y = y)) +
-  theme(
-    axis.ticks = element_line(colour = "white"),
-    axis.text = element_text(colour = "white"),
-    axis.title = element_text(colour = "white"),
-    panel.background = element_rect(fill = "white")
-    ) +
-  annotate(geom = "rect", xmin = 10, xmax = 20, ymin = 10, ymax =15,
+df %>% ggplot(mapping = aes(x = x, y = y)) +
+  # theme(
+  #   axis.ticks = element_line(colour = "white"),
+  #   axis.text = element_text(colour = "white"),
+  #   axis.title = element_text(colour = "white")
+  #   ) +
+  annotate(geom = "rect", xmin = 10, xmax = 20, ymin = 10, ymax = 15,
            fill = "red")
